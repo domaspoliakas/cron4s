e38951ef94576964bc1aeb6fa8604e4fe91bf0b7
Dmitriy Selyutin <gingerbreadmanphoto@gmail.com>
2023-02-15 14:07:08 +0100

    sbt files update

--- project/GithubWorkflow.scala
+++ project/GithubWorkflow.scala
@@ -1,17 +1,31 @@
+import sbt.Keys.publishTo
+import sbt.ThisBuild
 import sbtghactions.GenerativePlugin.autoImport._
+import sbtghpackages.GitHubPackagesKeys.{githubOwner, githubRepository}
+import xerial.sbt.Sonatype.autoImport.{sonatypeProjectHosting, sonatypePublishToBundle}
+import xerial.sbt.Sonatype.GitHubHosting
 
 object GithubWorkflow {
-  val DefaultJVM = "adopt@1.8"
+  val SupportedScalaVersions = Seq("2.13.6", "2.12.14")
+  val DefaultJVM             = "adopt@1.8"
 
   val JvmCond = s"matrix.platform == 'jvm'"
   val JsCond  = s"matrix.platform == 'js'"
 
+  ThisBuild / sonatypeProjectHosting := Some(
+    GitHubHosting("precog", "cron4s", "bot@precog.com")
+  )
+  ThisBuild / publishTo := sonatypePublishToBundle.value
+  ThisBuild / githubWorkflowPublishTargetBranches := Nil
+  ThisBuild / githubWorkflowScalaVersions := SupportedScalaVersions
+  ThisBuild / githubWorkflowJavaVersions := Seq("amazon-corretto@1.17")
+  ThisBuild / githubOwner := "precog"
+  ThisBuild / githubRepository := "cron4s"
+
   def settings =
     Seq(
-      githubWorkflowJavaVersions := Seq(DefaultJVM, "adopt@1.11", "adopt@1.15"),
       githubWorkflowTargetBranches := Seq("master"),
       githubWorkflowTargetTags ++= Seq("v*"),
-      githubWorkflowPublishTargetBranches := Seq(RefPredicate.StartsWith(Ref.Tag("v"))),
       githubWorkflowPublish := Seq(
         WorkflowStep.Sbt(
           List("ci-release"),
@@ -40,11 +54,6 @@ object GithubWorkflow {
           name = Some("Validate JVM"),
           cond = Some(JvmCond)
         )
-        /*WorkflowStep.Sbt(
-          List("clean", "binCompatCheck"),
-          name = Some("Binary compatibility ${{ matrix.scala }}"),
-          cond = Some(JvmCond)
-        )*/
       )
     )
 
